93cf7d0bbc0a3f23ee13ff044a78ecb770a2fdc9
leejoe03 <joelee2003@gmail.com>
2024-07-29 20:33:07 +1000

    first commit

---
+++ backend/dao/reviewsDAO.js
@@ -0,0 +1,77 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+
+let reviews
+
+export default class ReviewsDAO {
+    static async injectDB(conn) {
+        if (reveiws) {
+            return
+        }
+        try {
+            reviews = await conn.db("reviews").collection("reviews")
+        } catch (e) {
+            console.error(`Unable to establish collection handles in userDAO: ${e}`)
+        }
+    }
+
+    static async addReview(movieId, user, review) {
+        try {
+            const reviewDoc = {
+                movieId: movieId,
+                user: user,
+                review: review,
+            }
+            return await reviews.insertOne(reviewDoc)
+        } catch (e) {
+            console.error(`Unable to post review: ${e}`)
+            return { error: e }
+        }
+    }
+
+    static async getReview(reviewId) {
+        try {
+            return await reviews.findOne({_id: ObjectId(reviewId)})
+        } catch (e) {
+            console.error(`Uable to get review: ${e}`)
+            return { error: e }
+        }
+    }
+
+    static async updateReview(reviewId, user, review) {
+        try{
+            const updateResponse = await reviews.updateOne(
+                {_id: ObjectId(reviewId)},
+                { $set: {user: user, rewview: review}}
+            )
+            return updateResponse
+        } catch (e) {
+            console.error(`Uable to update review: ${e}`)
+            return { error: e }
+        }
+    }
+
+    static async deleteReview(reviewId) {
+        try{
+            const deleteReponse = await reviews.deleteOne(
+                {_id: ObjectId(reviewId)}
+            )
+            return deleteReponse
+        } catch (e) {
+            console.error(`Uable to delete review: ${e}`)
+            return { error: e }
+        }
+    }
+
+    static async getReviewsByMovieId(movieId) {
+        try{
+            const cursor = await reviews.find(
+                {movieId: parseInt(movieId)}
+            )
+            return cursor.toArray()
+        } catch (e) {
+            console.error(`Uable to get review: ${e}`)
+            return { error: e }
+        }
+    }
+}
